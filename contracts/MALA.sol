2411154bf6319bcf1b94e3b068851ea40128e9cb
Francisco Rolotti <franrolotti1@gmail.com>
2025-04-26 16:14:58 +0200

    Initial commit with ERC-1155 basic token

---
+++ contracts/MALA.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC1155, Ownable {
+    constructor() ERC1155("https://myapi.com/api/item/{id}.json") {}
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+}
